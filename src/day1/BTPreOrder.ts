0c15efbd21d84fbbc1fe14cf00e7b687395e9898
Samad Ariyan <samadariyan@yahoo.com>
2024-04-01 19:45:17 +0330

    upto Doubly Linked List

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,19 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // base case
+    if (!curr) {
+        return path;
+    }
 
+    // recursing:
+    // pre
+    path.push(curr.value);
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    // post
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
